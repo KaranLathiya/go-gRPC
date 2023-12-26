98119c40ffe50a0e4157370a9c28820a8e7e0c9d
Karan Lathiya <2810karanlathiya@gmail.com>
2023-12-26 18:55:07 +0530

    proper response

--- server/main.go
+++ server/main.go
@@ -1,32 +1,13 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a server for Greeter service.
 package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	pb "grpc/protoc"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -52,13 +33,19 @@ func main() {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-
 func (s *server) ServerReply(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	var err error
 	log.Printf("Received: %v", req.SomeString)
-	return &pb.HelloResponse{}, errors.New("")
+	// time.Sleep(5 * time.Second)
+	return &pb.HelloResponse{Reply: fmt.Sprintf("Hello %v", req.SomeString)}, err
 }
 
 func (s *server) MemoryRequirments(ctx context.Context, req *pb.RequirmentsRequest) (*pb.SupplyResponse, error) {
-	log.Printf("Type: %v, Size: %v", req.Type, req.Size )
-	return &pb.SupplyResponse{}, errors.New("")
+	var err error
+	log.Printf("Type: %v, Size: %v", req.Type, req.Size)
+	availableResources := 10
+	if availableResources > int(req.Size) {
+		return &pb.SupplyResponse{SizeAvailable: 10}, err
+	}
+	return &pb.SupplyResponse{}, err
 }
